3ba54ef992e59485aa926e53d105967f9f8c572a
Chayti Saha <chaytisaha98@gmail.com>
2023-02-27 16:44:30 +0600

    commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 // DB_USER=jkrBookShop
 // DB_PASS=eqWC50TP9rkUxKUB
 
+
 // -----> mongodb uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iea9q.mongodb.net/?retryWrites=true&w=majority`
 
